84d3f674441b74bfc2beece21c30d5750903fe6b
elenaK <elenakallaur15@gmail.com>
2022-12-16 21:51:47 -0500

    almost

--- seminar2.py
+++ seminar2.py
@@ -6,12 +6,12 @@
 # n=int(input('введите число '))
 # for i in range(n):
 #     print(random.randrange(-100,100), end=', ')  
-# or
+# # or
 # n=int(input('введите число '))
 # a=1
 # for i in range (n):
 #     print(a, end=" ")
-#     a=a*(-3)
+#     a=a*-3
 
 # 12 Для натурального N создать словарь индекс-значение, состоящий из элементов последовательности 3N+1
 # пример n = 6
@@ -23,29 +23,71 @@
 #     value=int(i*3+1)
 #     print(i, end=': ')
 #     print(value, end=', ')
-# or 
+# #or 
 # d={a: (3*a+1) for a in range (n)}
 # print(d)
+# or
+
+# a={}
+# for i in range (1,n+1):
+#     a[i] = i*3+1
+# print(a)
 
 # 13 Напишите программу, в которой пользователь будет вводить две строки, а программа определять количество вхождений одной строки 
 # в другую
 
-line=str(input('введите две строки '))
-line2=str(input())
-l=len(line)
-l2=len(line2)
-j=0
-count=0
-if l>l2:
-    for i in range(l+1):
-        while j<l2:
-            if line2[j] == line[i]:
-                j+=1
-count+=1
-print(count)
-# else:
-#     for i in range(l):
-#         if line == line2:
-#             count+=1
-#     print(count)
-# 
+# line1=str(input('введите две строки '))
+# line2=str(input())
+# count=0
+# for i in range(len(line1)-1):
+#     if line1[i:i+len(line2)]==line2:
+#         count+=1
+# print(count)
+
+# or
+
+# print(line1.count(line2))
+
+# 14 Создать список случайных чисел. Найти максимальное количество одинаковых элементов. 10 в диапазоне от 2 до 5
+
+# list=[]
+# import random
+# for i in range(10):
+#     list.append(random.randrange(2,5))
+# print(list)
+# max=0
+# for i in range(10):
+#     if list.count(list[i])>max:
+#         max=list.count(list[i])
+# print(max)
+
+# or
+# import random
+# list=[]
+# max=0
+# for i in range(10):
+#     a = random.randint(2,5)
+#     list.append(a)
+# print(list)
+# for i in set(list):
+#     if list.count(i)>max:
+#         max=list.count(i)
+# print(max)
+
+
+
+  
+# 15 Пользователь вводит число N, программа ввыводит N строк, начиная с N * по убыванию
+
+# n = int(input('введите любое натуральное число '))
+# for i in range(n, 0, -1):
+#     print('*'*i)
+
+# or
+
+# n = int(input('введите любое натуральное число '))
+# for i in range(n):
+#     print('*'*n)
+#     n-=1
+
+
